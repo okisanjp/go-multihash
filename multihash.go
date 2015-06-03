e2dbd45753f51c07ca3f4fae1b7b7934242470ae
okisanjp <okisan.jp@gmail.com>
2015-06-03 10:43:00 +0900

    整形

--- multihash.go
+++ multihash.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"crypto/md5"
-    "crypto/sha1"
-    "flag" // コマンドラインのフラグ解析をするパッケージ
-    "fmt"
+	"crypto/sha1"
+	"flag" // コマンドラインのフラグ解析をするパッケージ
+	"fmt"
 )
 
 func main() {
-    flag.Parse() // 引数をパース
-    h := []byte(flag.Arg(0)) // 1個目の引数のみ使う
-    hash(h)
+	flag.Parse()             // 引数をパース
+	h := []byte(flag.Arg(0)) // 1個目の引数のみ使う
+	hash(h)
 }
 
-func hash(h []byte){
-    fmt.Printf("RAW  : %s\n",h) // 文字列として表示
-    fmt.Printf("MD5  : %x\n",md5.Sum(h)) // 
-    fmt.Printf("SHA1 : %x\n", sha1.Sum(h))
+func hash(h []byte) {
+	fmt.Printf("RAW  : %s\n", h)          // 文字列として表示
+	fmt.Printf("MD5  : %x\n", md5.Sum(h)) //
+	fmt.Printf("SHA1 : %x\n", sha1.Sum(h))
 }
